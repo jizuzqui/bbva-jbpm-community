b3cea3c12464086577df2413b35a444e0ac1d26e
jaime.izuzquiza <jaime.izuzquiza@gmail.com>
2020-03-15 23:52:51 +0100

    Se eliminan comentarios

--- run_jbpm_community.sh
+++ run_jbpm_community.sh
@@ -32,11 +32,4 @@ docker run -p 8080:8080 -p 8001:8001 -p 8082:8082 -p 9990:9990 \
     --mount source=jbpm-data,target=/opt/jboss/data/ \
     --mount source=jbpm-user-group-data,target=$JBOSS_HOME/standalone/configuration/ \
     -d --name $JBPM_CONTAINER_NAME $JBPM_IMAGE_NAME:$JBPM_BBVA_IMAGE_VERSION
-
-# docker run -p 8080:8080 -p 8001:8001 -p 8082:8082 -p 9990:9990 \
-#    -v $JBPM_PERSISTENT_DIR/logs/:$JBOSS_HOME/standalone/log/ \
-#    -v $JBPM_PERSISTENT_DIR/repositories/mvn_home/:/opt/jboss/.m2/ \
-#    -v $JBPM_PERSISTENT_DIR/repositories/:/opt/jboss/repositories/ \
-#    -v $JBPM_PERSISTENT_DIR/data/:/opt/jboss/data/ \
-#    -v $JBPM_PERSISTENT_DIR/user_group_data/:$JBOSS_HOME/standalone/configuration/ \
 #    -d --name $JBPM_CONTAINER_NAME $JBPM_IMAGE_NAME:$JBPM_BBVA_IMAGE_VERSION
