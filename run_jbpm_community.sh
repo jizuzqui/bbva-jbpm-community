972a156f96b95faaf0bfe473cc753743b33731e6
jaime.izuzquiza <jaime.izuzquiza@bbva.com>
2020-09-13 22:36:10 +0200

    mock-server port exposed

--- run_jbpm_community.sh
+++ run_jbpm_community.sh
@@ -28,11 +28,12 @@ mkdir -p $JBPM_PERSISTENT_DIR/mock-services
 docker pull $JBPM_IMAGE_NAME:$JBPM_BBVA_IMAGE_VERSION 
 
 # Runing jBPM docker image
-docker run -p 8080:8080 -p 8001:8001 -p 8082:8082 -p 9990:9990 \
-    -m 3600m --cpus=2 \
+docker run -p 8080:8080 -p 8001:8001 -p 8082:8082 -p 9990:9990 -p 8787:8787 -p 1080:1080 \
+    -m 4096m --cpus=2 \
     --mount source=jbpm-repositories,target=/opt/jboss/.m2/ \
     --mount source=jbpm-repositories,target=/opt/jboss/repositories/ \
     --mount source=jbpm-data,target=/opt/jboss/data/ \
     --mount source=jbpm-user-group-data,target=$JBOSS_HOME/standalone/configuration/ \
     --mount source=jbpm-mock-services,target=/opt/jboss/mock-server/services/ \
+    --mount source=jbpm-ssh-keys,target=/home/jboss/.ssh \
     -d --name $JBPM_CONTAINER_NAME $JBPM_IMAGE_NAME:$JBPM_BBVA_IMAGE_VERSION
