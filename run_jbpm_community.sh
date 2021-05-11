fd5919de6c3d0dfc04080bc9025213cba2fd4bf8
jaime.izuzquiza <jaime.izuzquiza@bbva.com>
2021-05-11 10:00:18 +0200

    Se variabiliza el número de cpus asignado al contenedor

--- run_jbpm_community.sh
+++ run_jbpm_community.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 JBPM_PERSISTENT_DIR=$1
-JBPM_BBVA_IMAGE_VERSION=$2
+# JBPM_BBVA_IMAGE_VERSION=$2
 JBPM_IMAGE_NAME=jizuzquiza/jbpm-bbva-holding
 JBPM_CONTAINER_NAME=jbpm-bbva-holding
 JBOSS_HOME=/opt/jboss/wildfly
+CPUS_NUMBER="$(cat /proc/cpuinfo | grep processor | wc -l)"
+CPUS_DIVISOR=2
+
+if [ $CPUS_NUMBER > 2  ]
+then
+	CPUS_CONTAINER=$((CPUS_NUMBER / CPUS_DIVISOR))
+fi
+echo "Número de cpus $CPUS_CONTAINER"
 
 if [ -z "$JBPM_PERSISTENT_DIR" ]
 then
@@ -15,8 +23,8 @@ then
         JBPM_BBVA_IMAGE_VERSION=latest
 fi
 
-docker stop $JBPM_CONTAINER_NAME
-docker rm $JBPM_CONTAINER_NAME
+#docker stop $JBPM_CONTAINER_NAME
+#docker rm $JBPM_CONTAINER_NAME
 
 mkdir -p $JBPM_PERSISTENT_DIR/logs
 mkdir -p $JBPM_PERSISTENT_DIR/repositories/mvn_home
@@ -25,11 +33,11 @@ mkdir -p $JBPM_PERSISTENT_DIR/user_group_data
 mkdir -p $JBPM_PERSISTENT_DIR/mock-services
 
 # Downloading latest image version...
-docker pull $JBPM_IMAGE_NAME:$JBPM_BBVA_IMAGE_VERSION 
+#docker pull $JBPM_IMAGE_NAME:$JBPM_BBVA_IMAGE_VERSION 
 
 # Runing jBPM docker image
-docker run -p 8080:8080 -p 8001:8001 -p 8082:8082 -p 9990:9990 -p 8787:8787 -p 1080:1080 -p 9092:9092 -p 8090:8090 \
-    -m 4096m --cpus=2.5 \
+#docker run -p 8080:8080 -p 8001:8001 -p 8082:8082 -p 9990:9990 -p 8787:8787 -p 1080:1080 -p 9092:9092 -p 8090:8090 \
+    -m 4096m --cpus=$CPUS_CONTAINER \
     --mount source=jbpm-repositories,target=/opt/jboss/.m2/ \
     --mount source=jbpm-repositories,target=/opt/jboss/repositories/ \
     --mount source=jbpm-data,target=/opt/jboss/data/ \
